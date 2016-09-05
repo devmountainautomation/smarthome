2b281bf600c47e8077d858b61e341b2cb38789b0
rnoelle <reidnoelle2@gmail.com>
2016-09-05 17:49:41 -0600

    trying to pull

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -3,15 +3,4 @@ angular.module('smarthome')
 
         $urlRouterProvider.otherwise('/');
 
-        $stateProvider
-          .state('login', {
-            url: '/login',
-            templateUrl: './app/component/login/login.html'
-          })
-          .state('landing page', {
-              url: '/',
-              templateUrl: './app/component/landingPage/landingPage.html',
-              controller: 'landingCtrl'
-          })
-
     });
