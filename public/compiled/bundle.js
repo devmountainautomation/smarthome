f0f5b2319de2460ee24985885ed91e5911a14fa3
rnoelle <reidnoelle2@gmail.com>
2016-09-06 12:30:33 -0600

    prepping for pull

--- public/compiled/bundle.js
+++ public/compiled/bundle.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('smarthome', [ui.router]);
+angular.module('smarthome', ['ui.router']);
 'use strict';
 
 angular.module('smarthome').config(function ($stateProvider, $urlRouterProvider) {
