08a11e52647810ae90301a711a91844239ca4942
Steven Isbell <steveni_92@msn.com>
2016-09-07 09:14:01 -0600

    pre-merge commit

--- public/compiled/bundle.js
+++ public/compiled/bundle.js
@@ -17,6 +17,53 @@ angular.module('smarthome').config(function ($stateProvider, $urlRouterProvider)
 });
 'use strict';
 
+angular.module('smarthome').directive('headDir', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: './app/component/header/header.html',
+        link: function link(scope, elems, attrs) {
+            $(document).ready(function () {
+                $('.logged-in').hide();
+                $('#hamburger').click(function () {
+                    $('#hamburger').toggleClass('open');
+                    $('#menu').toggle('slide', 'left', 500);
+                    $('.landing-page').toggleClass('menu-open');
+                });
+
+                $(window).on('scroll', function () {
+                    if ($(window).scrollTop() > 50) {
+                        $('.header').addClass('active');
+                        $('.ham-slide').addClass('span-invert');
+                    } else {
+                        $('.header').removeClass('active');
+                        $('.ham-slide').removeClass('span-invert');
+                    }
+                });
+            });
+        }
+    };
+});
+'use strict';
+
+angular.module('smarthome').directive('landingDir', function () {
+    return {
+        restrict: 'EA',
+        link: function link(scope, elem, attrs) {
+            $(document).ready(function () {
+                // $(window).scroll(() => {
+                //     let winScroll = $(window).scrollTop() - 35;
+                //     if (winScroll < $('.landing-banner').offset().top - ($(window).height() / 3)) {
+                //         console.log(winScroll);
+                //         $('.skew-right').css("transform", "skewY(" + (winScroll / 2) + "deg)");
+                //         $('.skew-left').css("transform", "skewY(" + (-winScroll / 2) + "deg)");
+                //     }
+                // });
+            });
+        }
+    };
+});
+'use strict';
+
 angular.module('smarthome').directive('compareTo', function () {
   return {
     restrict: 'A',
@@ -112,51 +159,4 @@ angular.module('smarthome').directive('signupForm', function () {
       });
     }
   };
-});
-'use strict';
-
-angular.module('smarthome').directive('headDir', function () {
-    return {
-        restrict: 'EA',
-        templateUrl: './app/component/header/header.html',
-        link: function link(scope, elems, attrs) {
-            $(document).ready(function () {
-                $('.logged-in').hide();
-                $('#hamburger').click(function () {
-                    $('#hamburger').toggleClass('open');
-                    $('#menu').toggle('slide', 'left', 500);
-                    $('.landing-page').toggleClass('menu-open');
-                });
-
-                $(window).on('scroll', function () {
-                    if ($(window).scrollTop() > 50) {
-                        $('.header').addClass('active');
-                        $('.ham-slide').addClass('span-invert');
-                    } else {
-                        $('.header').removeClass('active');
-                        $('.ham-slide').removeClass('span-invert');
-                    }
-                });
-            });
-        }
-    };
-});
-'use strict';
-
-angular.module('smarthome').directive('landingDir', function () {
-    return {
-        restrict: 'EA',
-        link: function link(scope, elem, attrs) {
-            $(document).ready(function () {
-                // $(window).scroll(() => {
-                //     let winScroll = $(window).scrollTop() - 35;
-                //     if (winScroll < $('.landing-banner').offset().top - ($(window).height() / 3)) {
-                //         console.log(winScroll);
-                //         $('.skew-right').css("transform", "skewY(" + (winScroll / 2) + "deg)");
-                //         $('.skew-left').css("transform", "skewY(" + (-winScroll / 2) + "deg)");
-                //     }
-                // });
-            });
-        }
-    };
 });
