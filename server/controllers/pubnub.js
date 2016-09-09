162bbeae4f2d3510f4d0d25e7bee84efff94fb8b
crander7 <andersen.craigm@gmail.com>
2016-09-09 03:01:26 -0600

    Got IIFE working for pubnub users

--- server/controllers/pubnub.js
+++ server/controllers/pubnub.js
@@ -2,11 +2,46 @@ const app = require('../index.js')
 const Pubnub = require('pubnub');
 const config = require('../config/config.js');
 const db = app.get('db');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+//************************* Don't Delete this!! ***********************//
+
+// (() => {
+//   db.get_all_users([], (err, users) => {
+//     users.forEach((e) => {
+//       pubnub = {};
+//       pubnub[e.id] = new Pubnub({
+//         subscribeKey: e.pubsub,
+//         publishKey: e.pubpub,
+//         ssl: true
+//       });
+//
+//       pubnub[e.id].addListener({
+//         message: message => {
+//           console.log("This is the message for id " + e.id + ":", message);
+//         },
+//         presence: presence => {
+//           console.log("This is the presence for id " + e.id + ":", presence);
+//         },
+//         status: status => {
+//           console.log("This is the status for id " + e.id + ":", status);
+//         }
+//       });
+//
+//       pubnub[e.id].subscribe({
+//         channels: [e.pubchan],
+//         withPresence: true
+//       });
+//     });
+//     module.exports = pubnub;
+//   });
+// })();
 
 var pubnub = new Pubnub({
   subscribeKey: config.SubscribeKey,
   publishKey: config.PublishKey,
-  secretKey: config.SecretKey,
+  // secretKey: config.SecretKey,
   ssl: true
 });
 
