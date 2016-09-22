45819288d92a83ca6203eabab5effa5ae3e1e0de
Steven Isbell <steveni_92@msn.com>
2016-09-21 23:51:08 -0600

    sidebar done, small styling fixes

--- public/app/component/about/aboutAnimate.js
+++ public/app/component/about/aboutAnimate.js
@@ -1,45 +1,45 @@
 angular.module('smarthome')
-  .directive('aboutDir', () => {
+  .directive('aboutDir', ($state) => {
     return {
       restrict: 'EA',
       controller: 'aboutCtrl',
       link: (scope, elem, attrs) => {
         $(document).ready(() => {
           $(window).scroll(function() {
+            if ($state.current.name === 'about') {
+              let winScroll = $(this).scrollTop();
 
-            let winScroll = $(this).scrollTop();
+              if (winScroll > $('#craig').offset().top - ($(window).height() / 1.5)) {
+                setTimeout(() => {
+                  $('#craig').addClass('is-showing');
+                }, 150);
+              }
 
-            if (winScroll > $('#craig').offset().top - ($(window).height() / 1.5)) {
-              setTimeout(() => {
-                $('#craig').addClass('is-showing');
-              }, 150);
-            }
+              if (winScroll > $('#jon').offset().top - ($(window).height() / 1.5)) {
+                setTimeout(() => {
+                  $('#jon').addClass('is-showing');
+                }, 150);
+              }
 
-            if (winScroll > $('#jon').offset().top - ($(window).height() / 1.5)) {
-              setTimeout(() => {
-                $('#jon').addClass('is-showing');
-              }, 150);
-            }
+              if (winScroll > $('#noelle').offset().top - ($(window).height() / 1.5)) {
+                setTimeout(() => {
+                  $('#noelle').addClass('is-showing');
+                }, 150);
+              }
 
-            if (winScroll > $('#noelle').offset().top - ($(window).height() / 1.5)) {
-              setTimeout(() => {
-                $('#noelle').addClass('is-showing');
-              }, 150);
-            }
+              if (winScroll > $('#steven').offset().top - ($(window).height() / 1.5)) {
+                setTimeout(() => {
+                  $('#steven').addClass('is-showing');
+                }, 150);
+              }
 
-            if (winScroll > $('#steven').offset().top - ($(window).height() / 1.5)) {
-              setTimeout(() => {
-                $('#steven').addClass('is-showing');
-              }, 150);
-            }
-
-            if (winScroll > $('#semo').offset().top - ($(window).height() / 1.5)) {
-              setTimeout(() => {
-                $('#semo').addClass('is-showing');
-              }, 150);
+              if (winScroll > $('#semo').offset().top - ($(window).height() / 1.5)) {
+                setTimeout(() => {
+                  $('#semo').addClass('is-showing');
+                }, 150);
+              }
             }
           });
-
         });
       }
     };
--- public/app/component/sidebar/sideAnimate.js
+++ public/app/component/sidebar/sideAnimate.js
@@ -17,6 +17,11 @@ angular.module('smarthome')
             $('body').css('overflow-y', 'auto');
           });
 
+          $('.side-burger').on('click', () => {
+            $('.sidebar-hidden').toggle('slide', 'left', 300);
+          });
+
+
         });
       }
     };
