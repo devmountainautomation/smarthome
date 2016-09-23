045a34d1970eb55488e71519fd0227fca2bb155c
Steven Isbell <steveni_92@msn.com>
2016-09-22 20:45:48 -0600

    fixed animations

--- public/app/component/about/aboutAnimate.js
+++ public/app/component/about/aboutAnimate.js
@@ -33,13 +33,30 @@ angular.module('smarthome')
                 }, 150);
               }
 
-              // if (winScroll > $('#semo').offset().top - ($(window).height() / 1.5)) {
-              //   setTimeout(() => {
-              //     $('#semo').addClass('is-showing');
-              //   }, 150);
-              // }
+              if (winScroll > $('#semo').offset().top - ($(window).height() / 1.5)) {
+                setTimeout(() => {
+                  $('#semo').addClass('is-showing');
+                }, 150);
+              }
 
-              // $('.media-svg').css('transform', 'translateX(' + (winScroll/2) + 'px)');
+              if (winScroll > $('.media-svg').offset().top - ($(window).height() / 1.5)) {
+                $('.media-svg').each((i) => {
+                  setTimeout(() => {
+                    $('.media-svg').eq(i).addClass('is-showing');
+                  }, 150 * (i + 1));
+                });
+              }
+
+              if (winScroll > $('.this-is-it').offset().top - ($(window).height() / 1.5)) {
+                $('.this-is-it').each((i) => {
+                  setTimeout(() => {
+                    $('.this-is-it').eq(i).addClass('is-showing');
+                  }, 250 * (i + 1));
+                });
+              }
+
+
+
             }
           });
         });
